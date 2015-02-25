4dd6d0063164565a75cafc483a56d6dc7e655b24
ca2527705 <Usatashi@yahoo.com>
2015-02-25 10:40:05 -0800

    Signed-off-by: ca2527705 <Usatashi@yahoo.com>

---
+++ Lab/HelloWorld/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: Christopher Avalos
+ * Purpose: Sup
+ * Created on February 25, 2015, 10:39 AM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"Hello World"<<endl;
+
+    return 0;
+}
+
