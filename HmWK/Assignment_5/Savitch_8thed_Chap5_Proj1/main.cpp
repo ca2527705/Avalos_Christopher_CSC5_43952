0877882d8f2e1df4c07e68720a359c281b045a5d
usatashi <usatashi@yahoo.com>
2015-04-20 10:53:14 -0700

    Signed-off-by: usatashi <usatashi@yahoo.com>

---
+++ HmWK/Assignment_5/Savitch_8thed_Chap5_Proj1/main.cpp
@@ -0,0 +1,45 @@
+/*
+* File: main.cpp
+* Author: Christopher Avalos
+* Purpose: Converting 24hr to 12hr
+* Created on April 20, 2015, 9:45 AM
+*/
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global constants
+
+//Function prototypes
+
+void time(int&,int&);
+//read the military time
+void output(int,int);
+//show it to them
+
+int main(int argc, char** argv) {
+    int timehr,timemin;
+    time(timehr,timemin);
+    output(timehr,timemin);
+    return 0;
+}
+
+void time(int& timehr,int& timemin){
+    using namespace std;
+    cout<<"Insert the hr of the day in military time with two integers\n";
+    cin>>timehr;
+    cout<<"Insert the time in minutes with two integers\n";
+    cin>>timemin;
+}
+void output(int ntimehr,int ntimemin){
+    using namespace std;
+    if (ntimehr<=12){
+    cout<<"The time is "<<ntimehr<<":"<<ntimemin<<"AM"<<endl;
+}
+    
+else if (ntimehr-=12){
+    cout<<"The time is "<<ntimehr<<":"<<ntimemin<<"PM"<<endl;
+    }
+}
